a96a8302c236e0999b5c341cd4fb67d3ac26078f
Angel Bolaños <angelbs.1985@gmail.com>
2022-12-15 18:54:38 -0500

    cambio de pc

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-
+import { ComponentsModule } from '../components/components.module';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -26,8 +27,11 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
+    ComponentsModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    
   ]
 })
 export class PagesModule { }
